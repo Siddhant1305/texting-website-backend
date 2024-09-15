f984b46a377ba798231cbd3118fed00f980623bc
Siddhant Pandey <siddhantpandey1305@gmail.com>
2024-09-15 14:47:32 +0530

    Adding Middlewares

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const mongoose = require("mongoose");
 const path = require("path");
 const Chat = require("./models/chat.js");
 const methodOverride = require("method-override");
+const ExpressError = require("./ExpressError.js");
 
 app.set("views", path.join(__dirname, "views"));
 app.set("views engine", "ejs");
@@ -24,46 +25,84 @@ async function main() {
 
 // Index Route
 app.get("/chats", async (req, res) => {
-    let chats = await Chat.find();
-    // console.log(chats);
-    res.render("index.ejs", {chats});
+    try {
+        let chats = await Chat.find();
+        // console.log(chats);
+        res.render("index.ejs", {chats});
+    } catch (err) {
+      next(err);
+    }
 });
 
 // New Route
 app.get("/chats/new", (req, res) => {
+    // throw new ExpressError(404, "Page not Found");
     res.render("new.ejs");
 });
 
-// Create Route
-app.post("/chats", (req, res) => {
+// // Create Route - 1
+// app.post("/chats", async (req, res) => {
+//     let {from, to, msg} = req.body;
+//     let newChat = new Chat({
+//         from: from,
+//         to: to,
+//         msg: msg,
+//         created_at: new Date()
+//     });
+
+//     newChat
+//         .save()
+//         .then((res) => {
+//             console.log("chat was saved");
+//         })
+//         .catch((err) => {
+//             console.log(err);
+//         });
+//     res.redirect("/chats");
+// });
+
+
+// Create Route - 2
+app.post("/chats", asyncWrap(async (req, res, next) => {
+
     let {from, to, msg} = req.body;
     let newChat = new Chat({
         from: from,
         to: to,
         msg: msg,
         created_at: new Date()
-    })
+    });
 
-    newChat
-        .save()
-        .then((res) => {
-            console.log("chat was saved");
-        })
-        .catch((err) => {
-            console.log(err);
-        });
+    await newChat.save();
     res.redirect("/chats");
-});
+}));
+
+// AsyncWrap Function
+function asyncWrap(fn) {
+    return function(req, res, next) {
+        fn(req, res, next).catch(err => next(err));
+    };
+}
+
+// New - Show Route
+app.get("/chats/:id", asyncWrap (async (req, res, next) => {
+    let { id } = req.params;
+    let chat = await Chat.findById(id);
+    if (!chat) {
+        next(new ExpressError(404, "Chat not Found"));
+    }
+    res.render("edit.ejs", { chat });
+}));
 
 // Edit Route
-app.get("/chats/:id/edit", async (req, res) => {
+app.get("/chats/:id/edit", asyncWrap (async (req, res) => {
     let {id} = req.params;
     let chat = await Chat.findById(id);
     res.render("edit.ejs", { chat });
-});
+}));
 
 // Update Route
-app.put("/chats/:id", async (req, res) => {
+app.put("/chats/:id", asyncWrap (async (req, res) => {
     let {id} = req.params;
     let {msg: newMsg} = req.body;
     let updatedChat = await Chat.findByIdAndUpdate(
@@ -73,18 +112,42 @@ app.put("/chats/:id", async (req, res) => {
     );
     console.log(updatedChat);
     res.redirect("/chats");
-});
+}));
 
 // Delete Route
-app.delete("/chats/:id", async (req, res) => {
+app.delete("/chats/:id", asyncWrap(async (req, res) => {
     let {id} = req.params;
     let deletedChat = await Chat.findByIdAndDelete(id);
     console.log(deletedChat);
     res.redirect("/chats");
-});
+}));
 
+// Root Route
 app.get("/", (req, res) => {
     res.send("root is working");
+});
+
+
+const handleValidationError = (err) => {
+    console.log("This was a Validation error, Please follow Rules");
+    console.dir(err.message);
+    return err;
+}
+
+
+// Priniting Error Name - Error Handling Middleware
+app.use((err, req, res, next) => {
+    console.log(err.name);
+    if(err.name === "ValidationError") {
+        err = handleValidationError(err);
+    }
+    next(err);
+});
+
+// Error Handling Middleware
+app.use((err, req, res, next) => {
+    let {status=500, message="Some Error Occurred"} = err;
+    res.status(status).send(message);
 });
 
 app.listen(8080, () => {
